78aadada08c072ebb8417f2fe8f9f867dff3795c
Panayiotis Yiannoukkos <p.yiannoukkos@outlook.com>
2023-04-18 21:55:37 +0300

    sepparated mason-lspconfig and lspconfig, changed the way lsps are setup

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -14,3 +14,13 @@ mason.setup({
     }
 })
 
+local mason_lspconfig_status_ok, mason_lspconfig = pcall(require, 'mason-lspconfig')
+if not mason_lspconfig_status_ok then
+    return
+end
+
+mason_lspconfig.setup({
+    ensure_installed = { 'clangd', 'cmake', 'lua_ls', 'rust_analyzer', 'pylsp' },
+    automatic_installation = false,
+})
+
