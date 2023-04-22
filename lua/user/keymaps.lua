d522e2f35c86c172d98a3f85e87e18e9072ea742
Panayiotis Yiannoukkos <p.yiannoukkos@outlook.com>
2023-04-22 22:06:20 +0300

    add descriptions to keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -6,21 +6,32 @@ vim.keymap.set('', '<Space>', '<Nop>', opts)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set('i', 'kj', '<Esc>', opts) --> press kj fast to exit insert mode
-vim.keymap.set('n', '<leader>w', '<cmd>w!<CR>', opts) --> Save
-vim.keymap.set('n', '<leader>q', '<cmd>qa!<CR>', opts) --> Quit
-vim.keymap.set('n', '<leader>c', '<cmd>Bdelete!<CR>', opts) --> Close buffer
+vim.keymap.set('i', 'kj', '<Esc>', { desc = 'Exit insert mode' }) --> press kj fast to exit insert mode
+vim.keymap.set('n', '<leader>w', '<cmd>w!<CR>', { desc = 'Save File' }) --> Save
+vim.keymap.set('n', '<leader>q', '<cmd>qa!<CR>', { desc = 'Quit Neovim' }) --> Quit
+vim.keymap.set('n', '<leader>c', '<cmd>Bdelete!<CR>', { desc = 'Close Buffer' }) --> Close buffer
 
-vim.keymap.set('n', '<leader>pv', ':NvimTreeToggle<CR>', opts) --> Open file explorer
+vim.keymap.set('n', '<leader>pv', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' }) --> Open file explorer
 
 -- Telescope
 local status_ok, builtin = pcall(require, 'telescope.builtin')
 if not status_ok then
     return
 end
-vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
-vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
+
+vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader>/', function()
+    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
+        previewer = false,
+    }))
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
+vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind existing [B]uffers' })
 
 -- keep the cursor in its position when using these commands
 vim.keymap.set('n', 'J', 'mzJ`z', opts)
@@ -33,8 +44,8 @@ vim.keymap.set('n', 'N', 'Nzzzv', opts)
 vim.keymap.set('n', 'Q', '<nop>', opts) --> Never press Q
 
 -- move lines up and down
-vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv', opts)
-vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv', opts)
+vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv', { desc = 'Move lines downwards', silent = true })
+vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv', { desc = 'Move lines upwards', silent = true })
 
 -- better window navigation
 -- now handled by vim-tmux-navigator plugin
@@ -44,12 +55,12 @@ vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv', opts)
 -- vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
 
 -- navigate buffers
-vim.keymap.set('n', '<S-l>', ':bnext<CR>', opts)
-vim.keymap.set('n', '<S-h>', ':bprev<CR>', opts)
+vim.keymap.set('n', '<S-l>', '<cmd>bnext<CR>', { desc = 'Next Buffer' })
+vim.keymap.set('n', '<S-h>', '<cmd>bprev<CR>', { desc = 'Prev Buffer' })
 
 -- stay in indent mode
-vim.keymap.set('v', '<', '<gv', opts)
-vim.keymap.set('v', '>', '>gv', opts)
+vim.keymap.set('v', '<', '<gv', { desc = 'Indent and stay in Visual Mode' })
+vim.keymap.set('v', '>', '>gv',{ desc = 'Indent and stay in Visual Mode' })
 
 -- better paste
-vim.keymap.set('v', 'p', '"_dP', opts)
+vim.keymap.set('v', 'p', '"_dP',{ desc = 'Better Paste' })
