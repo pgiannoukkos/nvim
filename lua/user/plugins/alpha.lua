694d13a4917f284aab913b6f20f6229ecfd4624b
Panayiotis Yiannoukkos <p.yiannoukkos@outlook.com>
2023-05-10 18:44:47 +0300

    update alpha icons

--- lua/user/plugins/alpha.lua
+++ lua/user/plugins/alpha.lua
@@ -10,11 +10,11 @@ return {
 
         dashboard.section.buttons.val = {
             dashboard.button('e', '  New file', ':lua vim.cmd.ene()<CR>'),
-            dashboard.button('f', '  Find file', ':Telescope find_files<CR>'),
+            dashboard.button('f', '󰈞  Find file', ':Telescope find_files<CR>'),
             dashboard.button('p', '  Find project', ':Telescope projects<CR>'),
-            dashboard.button('r', '  Recently used files', ':Telescope oldfiles<CR>'),
+            dashboard.button('r', '󰄉  Recently used files', ':Telescope oldfiles<CR>'),
             dashboard.button('c', '  Configuration', ':e $MYVIMRC<CR>'),
-            dashboard.button('q', '  Quit Neovim', ':qa<CR>'),
+            dashboard.button('q', '  Quit Neovim', ':qa<CR>'),
         }
 
         dashboard.section.footer.val = 'https://github.com/pgiannoukkos'
