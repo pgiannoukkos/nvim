e19f26f5a50d6edee386a71bf93cc37ea6c84080
Panayiotis Yiannoukkos <p.yiannoukkos@outlook.com>
2023-05-16 14:20:17 +0300

    python formatting and linting

--- lua/user/plugins/null-ls.lua
+++ lua/user/plugins/null-ls.lua
@@ -5,8 +5,13 @@ return {
 
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.rustfmt,
+                -- Formatters
+                null_ls.builtins.formatting.stylua,  -- lua
+                null_ls.builtins.formatting.rustfmt, -- rust
+                null_ls.builtins.formatting.black,   -- python
+
+                -- Linters
+                null_ls.builtins.diagnostics.flake8, -- python
             },
         })
     end,
